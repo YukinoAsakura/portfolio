d53cc18860eb9b0460a06a5c36e4c3bd6334689a
YukinoAsakura <95021295+YukinoAsakura@users.noreply.github.com>
2022-08-25 06:34:47 +0900

    add pageup

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
+//ドロップダウンメニュー機能
 function dropdown(){
     // #navi直下のli要素をマウスオーバー
     $("#menu").children("li").hover(function(){
       // 下層ナビゲーションの表示を切り替える
-      console.log("TEST")
       $(this).children("ul").stop().slideToggle(100);
     });
   };
 
-function dropdown_sp(){
+function touchHover(){
   $("body").on({
     "touchstart": function() {
       return $(this).off("mouseover mouseout");
@@ -19,14 +19,22 @@ function dropdown_sp(){
       return $(this).removeClass("hover");
     }
   }, ".touch-hover");
-  
-  // #navi直下のli要素をマウスオーバー
-  $("#menu").children("li").hover(function(){
-    // 下層ナビゲーションの表示を切り替える
-    console.log("TEST")
-    $(this).children("ul").stop().slideToggle(100);
-  });
+
 };
+
+//ページトップリンクの出現
+function PageTopAnime() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 150){
+		$('#page_top').removeClass('downMove');
+		$('#page_top').addClass('upMove');
+	}else{
+		if($('#page_top').hasClass('upMove')){
+			$('#page_top').removeClass('upMove');
+			$('#page_top').addClass('downMove');
+		}
+	}
+}
 
 var windowWidth = $(window).width();
 var windowSm = 768;
@@ -34,7 +42,18 @@ var windowSm = 768;
 $(window).on('load',function(){
   if (windowWidth <= windowSm) {
     dropdown_sp();
-} else {
-    dropdown();
-}
+} 
+dropdown();
+});
+
+
+$(window).scroll(function () {
+	PageTopAnime();
+});
+
+$('#page_top a').click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 500);
+    return false;
 });
